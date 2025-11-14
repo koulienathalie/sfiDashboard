195a6c75a12ddc5d2a0eabafc1871e2dae7125c6
Josoa Vonjiniaina <76913187+josoavj@users.noreply.github.com>
2025-11-14 14:29:08 +0300

    Create User.js

---
+++ backend/src/models/User.js
@@ -0,0 +1,14 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../databases/Sequelize');
+
+const User = sequelize.define('User', {
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  firstName: { type: DataTypes.STRING, allowNull: false },
+  lastName: { type: DataTypes.STRING },
+  email: { type: DataTypes.STRING, allowNull: false, unique: true },
+  password: { type: DataTypes.STRING, allowNull: false }
+}, {
+  modelName: 'User'
+});
+
+module.exports = { User };
