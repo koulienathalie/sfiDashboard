50921839c57578ffae385125b4907fa35e33dcc1
Josoa Vonjiniaina <76913187+josoavj@users.noreply.github.com>
2025-11-14 22:12:30 +0300

    Update User.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -6,7 +6,8 @@ const User = sequelize.define('User', {
   firstName: { type: DataTypes.STRING, allowNull: false },
   lastName: { type: DataTypes.STRING },
   email: { type: DataTypes.STRING, allowNull: false, unique: true },
-  password: { type: DataTypes.STRING, allowNull: false }
+  password: { type: DataTypes.STRING, allowNull: false },
+  role: { type: DataTypes.STRING, allowNull: false, defaultValue: 'user' }
 }, {
   modelName: 'User'
 });
